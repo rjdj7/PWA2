7667c9dfca36883a99ab368d1b9334df5f982a66
John Smith <rjdj7@yahoo.com>
2015-04-16 15:27:36 -0400

    Project Updated

--- Smith_John_Project/Smith_John_Project1/js/main.js
+++ Smith_John_Project/Smith_John_Project1/js/main.js
@@ -1,18 +1,43 @@
 /*  
-	Your Project Title
-	Author: You
+	PWA Project
+	Author: John Smith
 */
 
-(function($){
+$(document).ready(function(){
 	
-	
-	
-	
-		
 
-	
-})(jQuery); // end private scope
+	/* Tab Javascript ---------------------------- */
 
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+
+	/* Modal Javascript -------------------------- */
+
+	$(".modalClick").on("click", function(event) {
+		event.preventDefault();
+		$("#overlay")
+			.fadeIn()
+			.find("#modal")
+			.fadeIn();
+	});
+
+	$(".close").on("click", function(event) {
+		event.preventDefault();
+		$("#overlay")
+			.fadeOut()
+			.find("#modal")
+			.fadeOut();
+	});
+
+})
 
 
 
