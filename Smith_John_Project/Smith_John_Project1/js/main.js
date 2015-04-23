4a7b23e4e04efe2e34dbc37c6e491abbed799e9e
John Smith <rjdj7@yahoo.com>
2015-04-23 18:40:15 -0400

    Project Part 3 Completed

--- Smith_John_Project/Smith_John_Project1/js/main.js
+++ Smith_John_Project/Smith_John_Project1/js/main.js
@@ -37,7 +37,118 @@ $(document).ready(function(){
 			.fadeOut();
 	});
 
-})
+	/* SignIn Javascript ------------------------ */
+
+	$("#signinButton").click(function(){
+		var user = $("#user").val();
+		var pass = $("#pass").val();
+		console.log("This notifies you if the password is working.");
+		$.ajax({
+			url: "xhr/login.php",
+			type: "post",
+			dataType: "json",
+			data: {
+				username: user,
+				password: pass
+			},
+			success:function(response){
+				console.log("test user");
+				if (response.error){
+					alert(response.error);
+				}else{
+					window.location.assign("admin.html")
+				};
+			})
+		});
+	});
+
+	/* SignOut Javascript ------------------------ */
+
+	$("#logOut").click(function(e){
+		e.preventDefault;
+		$.get("xhr/logOut.php", function(){
+			window.location.assign("index.html")
+		})
+	});
+
+	/* Register Javascript ------------------------ */
+
+	$("#register").on("click", function(){
+		var firstname = $("#first").val(),
+			lastname = $("#last").val(),
+			username = $("#userName").val(),
+			email = $("#email").val(),
+			password = $("#password").val();
+			console.log(firstname + ' ' + lastname + ' ' + username + ' ' + email + ' ' + password);
+
+		$.ajax({
+			url: "xhr/register.php",
+			type: "post",
+			dataType: "json",
+			data: {
+				firstname: firstname,
+				lastname: lastname,
+				username: username,
+				email: email,
+				password: password
+			},
+
+			success: function(response){
+				if (response.error){
+					alert(response.error);
+				}else{
+					window.location.assign("admin.html");
+				}
+			}
+		});
+	});
+
+	/* Projects Button Javascript ------------------------ */
+
+	$(".projectsbtn").on("click", function(e) {
+		e.preventDefault();
+		window.location.assign("projects.html");
+	});
+
+	/* Display Username Javascript ------------------------ */
+
+	$.getJSON("xhr/check_login.php", function(data){
+		console.log(data);
+		$.each(data, function(key, val){
+			console.log(val.first_name);
+			$(".userid").html("Welcome User: " + val.first_name);
+		})
+	});
+
+	/* Add New Projects Javascript ------------------------ */
+
+	$("#addButton").on("click", function(){
+		var projName = $("#projectName").val(),
+		projDesc = $("#projectDescription").val(),
+		projDue = $("#projectDueDate").val(),
+		status = $("input[name = 'status']:checked").prop("id");
+
+		$.ajax({
+			url: "xhr/new_project.php",
+			type: "post",
+			dataType: "json",
+			data: {
+				projectName: projName,
+				projectDescripion: projDesc,
+				dueDate: projDue,
+				status: status
+			},
+			success: function(response) {
+				console.log("Testing for success");
+
+				if(response.error) {
+					alert(response.error);
+				} else {
+					window.location.assign("projects.html");
+				};
+			}
+		});
+	});
 
 
-
+});
